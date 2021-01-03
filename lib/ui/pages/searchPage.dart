f3a6f86bb8c4390d0a480054373a901e26a25aef
YuanRuQian <yuanruqian@outlook.com>
2021-01-03 19:32:51 +0800

    update search page

--- lib/ui/pages/searchPage.dart
+++ lib/ui/pages/searchPage.dart
@@ -1,5 +1,12 @@
 
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:view/models/userData.dart';
+import 'package:view/models/userModel.dart';
+import 'package:view/ui/pages/profilePage.dart';
+import 'package:view/services/database.dart';
+import 'package:provider/provider.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -7,10 +14,96 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  TextEditingController _searchController = TextEditingController();
+  Future<QuerySnapshot> _users;
+
+  _buildUserTile(User user) {
+    return ListTile(
+      leading: CircleAvatar(
+        radius: 20.0,
+        backgroundImage: user.profileImageUrl.isEmpty
+            ? AssetImage('assets/images/user_placeholder.jpg')
+            : CachedNetworkImageProvider(user.profileImageUrl),
+      ),
+      title: Text(user.name),
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (_) => ProfilePage(
+            currentUserId: Provider.of<UserData>(context).currentUserId,
+            userId: user.id,
+          ),
+        ),
+      ),
+    );
+  }
+
+  _clearSearch() {
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => _searchController.clear());
+    setState(() {
+      _users = null;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: TextField(
+          controller: _searchController,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+            border: InputBorder.none,
+            hintText: 'Search',
+            prefixIcon: Icon(
+              Icons.search,
+              size: 30.0,
+            ),
+            suffixIcon: IconButton(
+              icon: Icon(
+                Icons.clear,
+              ),
+              onPressed: _clearSearch,
+            ),
+            filled: true,
+          ),
+          onSubmitted: (input) {
+            if (input.isNotEmpty) {
+              setState(() {
+                _users = DatabaseService.searchUsers(input);
+              });
+            }
+          },
+        ),
+      ),
+      body: _users == null
+          ? Center(
+              child: Text('Search for a user'),
+            )
+          : FutureBuilder(
+              future: _users,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (snapshot.data.documents.length == 0) {
+                  return Center(
+                    child: Text('No users found! Please try again.'),
+                  );
+                }
+                return ListView.builder(
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    User user = User.fromDoc(snapshot.data.documents[index]);
+                    return _buildUserTile(user);
+                  },
+                );
+              },
+            ),
     );
   }
 }
