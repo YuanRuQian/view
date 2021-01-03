b9388c228863fe33f762d88d84eb58ca61f355ef
YuanRuQian <yuanruqian@outlook.com>
2021-01-03 18:23:32 +0800

    update comment page

--- lib/ui/pages/commentPage.dart
+++ lib/ui/pages/commentPage.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:view/models/postModel.dart';
+import 'package:view/models/userModel.dart';
+import 'package:view/models/userData.dart';
+import 'package:view/models/commentModel.dart';
+import 'package:view/services/database.dart';
+import 'package:view/utilities/constants.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 class CommentPage extends StatefulWidget {
   final Post post;
@@ -12,10 +20,144 @@ class CommentPage extends StatefulWidget {
 }
 
 class _CommentPageState extends State<CommentPage> {
+
+    final TextEditingController _commentController = TextEditingController();
+  bool _isCommenting = false;
+  
+  _buildComment(Comment comment) {
+    return FutureBuilder(
+      future: DatabaseService.getUserWithId(comment.authorId),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (!snapshot.hasData) {
+          return SizedBox.shrink();
+        }
+        User author = snapshot.data;
+        return ListTile(
+          leading: CircleAvatar(
+            radius: 25.0,
+            backgroundColor: Colors.grey,
+            backgroundImage: author.profileImageUrl.isEmpty
+                ? AssetImage('assets/images/user_placeholder.jpg')
+                : CachedNetworkImageProvider(author.profileImageUrl),
+          ),
+          title: Text(author.name),
+          subtitle: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(comment.content),
+              SizedBox(height: 6.0),
+              Text(
+                DateFormat.yMd().add_jm().format(comment.timestamp.toDate()),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  _buildCommentTF() {
+    final currentUserId = Provider.of<UserData>(context).currentUserId;
+    return IconTheme(
+      data: IconThemeData(
+        color: _isCommenting
+            ? Theme.of(context).accentColor
+            : Theme.of(context).disabledColor,
+      ),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(width: 10.0),
+            Expanded(
+              child: TextField(
+                controller: _commentController,
+                textCapitalization: TextCapitalization.sentences,
+                onChanged: (comment) {
+                  setState(() {
+                    _isCommenting = comment.length > 0;
+                  });
+                },
+                decoration:
+                    InputDecoration.collapsed(hintText: 'Write a comment...'),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () {
+                  if (_isCommenting) {
+                    DatabaseService.commentOnPost(
+                      currentUserId: currentUserId,
+                      post: widget.post,
+                      comment: _commentController.text,
+                    );
+                    _commentController.clear();
+                    setState(() {
+                      _isCommenting = false;
+                    });
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          'Comments',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(12.0),
+            child: Text(
+              '${widget.likeCount} likes',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          StreamBuilder(
+            stream: commentsRef
+                .document(widget.post.id)
+                .collection('postComments')
+                .orderBy('timestamp', descending: true)
+                .snapshots(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return Expanded(
+                child: ListView.builder(
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    Comment comment =
+                        Comment.fromDoc(snapshot.data.documents[index]);
+                    return _buildComment(comment);
+                  },
+                ),
+              );
+            },
+          ),
+          Divider(height: 1.0),
+          _buildCommentTF(),
+        ],
+      ),
     );
   }
 }
